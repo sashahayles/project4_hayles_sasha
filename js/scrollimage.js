6f5a89037e6139be0e1d63fa85167ce095c91325
Sasha Hayles <sashahayles@yahoo.com>
2019-08-08 17:48:12 -0400

    playing with parallax in new dummy page

---
+++ js/scrollimage.js
@@ -0,0 +1,7 @@
+$('.parallax-window').parallax({imageSrc: 'images/ivory.jpg.jpg'});
+jQuery(window).trigger('resize').trigger('scroll');
+
+$('.parallax-window').parallax({
+    naturalWidth: 600,
+    naturalHeight: 400
+  });
